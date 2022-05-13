12a1aec7c34f8dfa1b29ca6df098cdc7bb190387
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2022-05-13 13:51:26 +0300

    0.0.2

--- src/db/add_data.py
+++ src/db/add_data.py
@@ -1,21 +1,20 @@
 from db.create_random_data import get_groups, subject, get_random_student_first_name, get_random_student_last_name, \
     get_random_digit_in_range
-from src.db.models import db
+from src.db.models import db, courses
 from src.db.models import Group, Student, Course
 
 # create database and the db tables
-# db.create_all()
-#
-# # insert data
-# for _ in range(10):
-#     db.session.add(Group(get_groups()))
-#     db.session.commit()
-#
-#
-# for k, v in subject.items():
-#     db.session.add(Course(k, v))
-#     db.session.commit()
-#
+db.create_all()
+
+# insert data
+for _ in range(10):
+    db.session.add(Group(get_groups()))
+    db.session.commit()
+
+for k, v in subject.items():
+    db.session.add(Course(k, v))
+    db.session.commit()
+
 j = 1
 for i in (get_random_digit_in_range(200, 10, 10, 30)):
     count = 1
@@ -24,8 +23,9 @@ for i in (get_random_digit_in_range(200, 10, 10, 30)):
         db.session.commit()
         count += 1
     j += 1
-#
-# # db.session.add(courses(ge))
+
+db.session.add(courses())
+db.session.commit()
 #
 # # commit the changes to db
 # db.session.commit()
--- src/db/models.py
+++ src/db/models.py
@@ -5,16 +5,17 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:admin@localhost/uni'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-# courses = db.Table('courses',
-#                    db.Column('student_id', db.Integer, db.ForeignKey('student.id'), primary_key=True),
-#                    db.Column('id', db.Integer, db.ForeignKey('course'), primary_key=True)
-#                    )
+
+courses = db.Table('courses',
+                   db.Column('student_id', db.Integer, db.ForeignKey('student.id'), primary_key=True),
+                   db.Column('course_id', db.Integer, db.ForeignKey('course.id'), primary_key=True)
+                   )
 
 
 class Group(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(5), unique=True)
-    # students = db.relationship('Student', backref='group', lazy=True)
+    students = db.relationship('Student', backref='group', lazy=True)
 
     def __init__(self, name):
         self.name = name
@@ -24,22 +25,20 @@ class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(25), nullable=False)
     last_name = db.Column(db.String(25), nullable=False)
-    group_id = db.Column(db.Integer, nullable=True)
-    # group_id = db.Column(db.Integer, db.ForeignKey('group.id'), nullable=False)
-    # course_id = db.relationship('Course', secondary=courses, lazy='subquery',
-    #                             backref=db.backref('students', nullable=False, lazy=True))
+    group_id = db.Column(db.Integer, db.ForeignKey('group.id'), nullable=False)
+    course_id = db.relationship('Course', secondary=courses, lazy='subquery',
+                                backref=db.backref('student', nullable=False, lazy=True))
 
-    def __init__(self, first_name, last_name, id_group):
+    def __init__(self, first_name, last_name, group_id):
         self.first_name = first_name
         self.last_name = last_name
-        self.id_group = id_group
+        self.group_id = group_id
 
 
 class Course(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(25), unique=True, nullable=False)
     description = db.Column(db.String(250))
-
 
     def __init__(self, name, description):
         self.name = name
